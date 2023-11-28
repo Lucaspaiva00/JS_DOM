bdd7bbbf4d7f69857bab03f73676f2bb4123f447
LUCAS PAIVA <46923946+Lucaspaiva00@users.noreply.github.com>
2023-11-28 17:03:14 -0300

    Add files via upload

--- NUBANKI - PROJ FINAL/script/js.js
+++ NUBANKI - PROJ FINAL/script/js.js
@@ -5,46 +5,56 @@ const total_inv = document.querySelector("h2#total_inv");
 const poupanca = document.querySelector("h2#poupanca");
 const prefixado = document.querySelector("h2#prefixado");
 
-let contador = 250;
+let contador = 0;
+let cal = 0;
+let cal_mes = 0;
 
 function calc() {
 
     contador += 250;
-
-    valor_inic.innerHTML = `R$${contador},00`
+    cal += 250;
+    valor_inic.innerHTML = `R$${cal},00`
     res_final.innerHTML = `R$${contador * 0.006 + contador},00`;
-    total_inv.innerHTML = `Total investido: R$${contador * 0.014 + contador},00`;
+    total_inv.innerHTML = `Total investido: R$${cal},00`;
     poupanca.innerHTML = `Na poupanca, seu dinheiro renderia:  R$${contador * 0.001 + contador},00`;
     prefixado.innerHTML = `No Tesouro Prefixado, seu dinheiro renderia: R$${contador * 0.006 + contador},00`;
 }
 
 function diminu() {
     contador -= 250;
-
-    valor_inic.innerHTML = `R$${contador},00`
-    res_final.innerHTML = `R$${contador * 2},00`
+    cal -= 250;
+    valor_inic.innerHTML = `R$${cal},00`
+    res_final.innerHTML = `R$${contador},00`
     if (contador < 0) {
         valor_inic.innerHTML = `R$0,00`
         res_final.innerHTML = `R$0,00`
+    }
+    if (cal < 0) {
+        cal = 0;
     }
 }
 
 function calc_mes() {
     contador += 250;
-    valor_mes.innerHTML = `R$${contador},00`
-    res_final.innerHTML = `R$${contador * 0.1 + contador},00`;
+    cal_mes += 250;
+    valor_mes.innerHTML = `R$${cal_mes},00`
+    res_final.innerHTML = `R$${contador * 0.006 + contador},00`;
 
-    total_inv.innerHTML = `Total investido: R$${contador * 0.014 + contador},00`;
+    total_inv.innerHTML = `Total investido: R$${cal + cal_mes},00`;
     poupanca.innerHTML = `Na poupanca, seu dinheiro renderia:  R$${contador * 0.001 + contador},00`;
     prefixado.innerHTML = `No Tesouro Prefixado, seu dinheiro renderia: R$${contador * 0.006 + contador},00`;
 }
 
 function diminu_mes() {
     contador -= 250;
+    cal_mes -= 250;
     valor_mes.innerHTML = `R$${contador},00`
-    res_final.innerHTML = `R$${contador * 3},00`
+    res_final.innerHTML = `R$${contador},00`
     if (contador < 0) {
         valor_mes.innerHTML = `R$0,00`
         res_final.innerHTML = `R$0,00`
+    }
+    if (cal_mes < 0) {
+        cal_mes = 0;
     }
 }
